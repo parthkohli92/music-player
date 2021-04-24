0ab4f5a3629917ca33119946db4ee02193c79bb0
Parth Kohli <59307125+parthkohli92@users.noreply.github.com>
2021-04-24 16:30:23 +0530

    Music-player-1

---
+++ script.js
@@ -0,0 +1,98 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+// Music
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Goodnight, Disco Queen',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Front Row (Remix)',
+        artist: 'Metric/Jacinto Design'
+    },
+];
+
+
+// Check if playing
+let isPlaying = false;
+
+// play
+function playSong() {
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause')
+    music.play();
+}
+
+// Pause
+function pauseSong() {
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play')
+    music.pause();
+}
+
+// Play or Pause Event Listener
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+};
+
+// Current Song
+let songIndex = 0;
+
+// Prev song
+function prevSong () {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    } 
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// next song
+function nextSong () {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    } 
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// On Load - Select First Song
+loadSong(songs[songIndex]);
+
+
+
+
+// Event Listener
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
